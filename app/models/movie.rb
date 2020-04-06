6f2894428830beb5cf3a35228858b015a4b86ffd
Xiaoyi Liu <lxy878@gmail.com>
2020-04-06 08:44:55 +0000

    Automatically backed up by Learn

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,28 @@
 class Movie < ActiveRecord::Base
+  def can_be_instantiated_and_then_saved
+
+  end
+
+  def can_be_created_with_a_hash_of_attributes
+
+  end
+  
+  def can_be_created_in_a_block
+
+  end
+
+
+  def can_get_the_first_item_in_the_database
+
+  end
+
+  def can_get_the_last_item_in_the_database
+
+  end
+
+  def can_get_size_of_the_database
+
+  end
+
+
 end
