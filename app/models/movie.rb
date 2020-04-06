9b1ecb224461954e569603d7ec9925de9962c186
Xiaoyi Liu <lxy878@gmail.com>
2020-04-06 08:45:03 +0000

    Automatically backed up by Learn

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,12 +1,12 @@
 class Movie < ActiveRecord::Base
   def can_be_instantiated_and_then_saved
-
+    
   end
 
   def can_be_created_with_a_hash_of_attributes
 
   end
-  
+
   def can_be_created_in_a_block
 
   end
