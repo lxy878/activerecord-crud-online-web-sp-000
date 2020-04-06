9ccee75d45f98847f18492bc014c5a66a3ef6d32
Xiaoyi Liu <lxy878@gmail.com>
2020-04-06 09:29:13 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -76,9 +76,8 @@ end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
-  Movie.create(title: "Wat?")
-  __
-  __
+  movie = Movie.create(title: "Wat?")
+  movie 
 end
 
 def can_update_multiple_items_at_once
